a6a1be99b21c2183196c564b2a489f52749077e2
Sean Goresht <seangores@gmail.com>
2013-11-03 23:50:43 -0700

    Commit static html

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * GET home page.
- */
-
-exports.index = function (req, res)
-{
-	res.render("index", { title: "Drama 205: Final Project", description: "Interactive comic-style story-telling HTML5 application" });
-};
